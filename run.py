ea7645c7b8876769b2180910d03cb43fc591a980
roman-gs <roman_gs@hotmail.fr>
2022-12-15 16:47:32 +0000

    Collect Sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -49,23 +49,13 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the list data provided.
     """
-    print('Updating sales worksheet...')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated successfully.\n')
-
-def update_surplus_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print('Updating surplus worksheet...')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print('Surplus worksheet updated successfully.\n')
+    print(f'updating {worksheet} worksheet...\n')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f' {worksheet} worksheet updated successfully...\n')
 
 def calculate_surplus_data(sales_row):
     """
@@ -81,14 +71,30 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
+
+
 def main():
     """
     Run all program functions
     """   
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
